d781b2fa530d79e71c2b878b98d9bb8a24ee3b85
WFU <wfu@college.harvard.edu>
2019-02-14 18:05:09 -0500

    [tfmicro] works

--- tfmicro/README.md
+++ tfmicro/README.md
@@ -10,3 +10,11 @@ However, all of the default firmware is written in C. Therefore, in this segment
 we will compile TF Micro for the microcontoller using C++ (`arm-none-eabi-g++`),
 expose some libraries to C, and compile everything in the end in C so the
 default firmware works (`arm-non-eabi-gcc`).
+
+
+## Creating the TF Micro Model
+
+We will be trying to use fully connected layers, and for testing we will train
+a few models of varying size in `./train` and make sure that they work in
+the regular C++ Tensorflow Lite environment.
+
--- tfmicro/machinelearning.cpp
+++ tfmicro/machinelearning.cpp
@@ -1,16 +1,10 @@
-/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+/* An attempt at creating C extensions to TF-Micro in order to get it to run
+on a Crazyflie 2.0. The reason for this is because all of the firmware for
+the crazyflie is written in C, and although we can compile TF-Micro for the
+Cortex M4, we still have to link it to the main loop in the firmware.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Approach will be to expose the common functions we need in C, compile TF Micro
+in C++, and then link it to the main loop later.
 ==============================================================================*/
 
 #include "tensorflow/lite/experimental/micro/examples/micro_speech/audio_provider.h"
@@ -25,8 +19,6 @@ limitations under the License.
 #include "tensorflow/lite/version.h"
 
 int main(int argc, char* argv[]) {
-  // Set up logging.
-  tflite::MicroErrorReporter micro_error_reporter;
   tflite::ErrorReporter* error_reporter = &micro_error_reporter;
 
   // Map the model into a usable data structure. This doesn't involve any
